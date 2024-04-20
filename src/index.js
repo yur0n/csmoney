b95bddbb962de4fb6ade95248d3ccea7697cb98b
yur0n <yur0n@yahoo.com>
2024-04-20 07:47:10 +0300

    up

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import socket from './socket.js';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-import http from 'http';
-const httpServer = http.createServer(app);
-const server = httpServer.listen(3000, () => {
-  console.log('HTTP Server running on port: 3000');
-});
-
-// const port = process.env.PORT || 443;
-
-// const privateKey = readFileSync(join(import.meta.dirname, '../tls/key.pem'), 'utf8');
-// const certificate = readFileSync(join(import.meta.dirname, '../tls/cert.pem'), 'utf8');
-
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate
-// };
-
-// const httpsServer = https.createServer(credentials, app);
-
-// const server = httpsServer.listen(port, () => {
-//   console.log('HTTPS Server running on port: ' + port);
+// import http from 'http';
+// const httpServer = http.createServer(app);
+// const server = httpServer.listen(3000, () => {
+//   console.log('HTTP Server running on port: 3000');
 // });
+
+const port = process.env.PORT || 443;
+
+const privateKey = readFileSync(join(import.meta.dirname, '../tls/key.pem'), 'utf8');
+const certificate = readFileSync(join(import.meta.dirname, '../tls/cert.pem'), 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate
+};
+
+const httpsServer = https.createServer(credentials, app);
+
+const server = httpsServer.listen(port, () => {
+  console.log('HTTPS Server running on port: ' + port);
+});
 
 socket(server);
